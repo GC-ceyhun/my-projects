8257297e808aae7542bca211d1d18d1069fa0883
Gökay Ceyhun Şanlıoğlu <90559902+GC-ceyhun@users.noreply.github.com>
2023-03-29 17:07:51 +0300

    Add files via upload

---
+++ WebProject_2/level-3/uygulama-2(not)/uygulama-2.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("h1")[0].style.fontSize="120px";
+document.getElementsByClassName("h2")[1].style.fontSize="120px";
+document.getElementsByClassName("h1")[0].style.color="white";
+document.getElementsByClassName("h2")[1].style.color="white";
